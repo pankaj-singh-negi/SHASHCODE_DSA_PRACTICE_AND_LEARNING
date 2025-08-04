e6fac9a7211c3af2b2da1bd850ab136a32026438
pankaj-singh-negi <dsashayarpankaj@gmail.com>
2025-08-04 11:32:48 +0530

    code_updated

--- MATHS_FOR_DSA_PART_1/Reverse_Number.java
+++ MATHS_FOR_DSA_PART_1/Reverse_Number.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Reverse_Number {
     
         public static void main(String[] args) {
-           Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number to reverse: ");
         int n = sc.nextInt();
 
@@ -13,15 +13,13 @@ public class Reverse_Number {
 
 
 public static int reverseDigits(int n) {
-        // Code here
-     int num = n;
-        int digit;
-        int reverse = 0;
-        while(num != 0){
-            digit = num%10;
-            reverse = reverse*10 + digit;
-            num = num/10;
-        }
-        return reverse;
+    int num = Math.abs(n);
+    int reverse = 0;
+    while (num != 0) {
+        int digit = num % 10;
+        reverse = reverse * 10 + digit;
+        num = num / 10;
     }
+    return n < 0 ? -reverse : reverse;
+}
 }
